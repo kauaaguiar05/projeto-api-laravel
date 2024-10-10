84433ba0f241194c65604fd81aa141050094ae40
kauaaguiar05 <kaua.aguiar@rede.ulbra.br>
2024-10-09 21:20:19 -0300

    mod

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/cliente', [ClienteController::class, 'listar']);
-
+Route::get('/cliente/{id}', [ClienteController::class, 'listaPeloId']);
+Route::post('/cliente', [ClienteController::class, 'salvar']);
+Route::put('/cliente/{id}', [ClienteController::class, 'editar']);
+Route::delete('/cliente/{id}', [ClienteController::class, 'deletar']);
